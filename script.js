ef0bc099971ec647cdcae0fb1a26cf3fd0ae2ea2
modiniraj09 <108391974+modiniraj09@users.noreply.github.com>
2022-08-15 16:36:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ function evaluatePostfix(s) {
 
 function prec(c) {
   if (c == "^") return 3;
-  else if (c == "/" || c == "*") return 2;
+  else if (c == "/" || c == "*" || c=="rem") return 2;
   else if (c == "+" || c == "-") return 1;
   else return -1;
 }
